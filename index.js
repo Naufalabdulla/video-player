f40e8959f3bf5c7dffac5b7b9e8eb14601ca3de7
Naufal_abdullah <lnaufa90@gmail.com>
2024-02-01 22:25:17 +0700

    another new

---
+++ index.js
@@ -0,0 +1,27 @@
+const playPauseBtn = document.querySelector('.play-pause-btn')
+const video = document.querySelector('video')
+const videoContainer = document.querySelector('.video-container')
+const lala = console.log('sasa')
+
+playPauseBtn.addEventListener('click', togglePlay)
+video.addEventListener('click', togglePlay)
+
+document.addEventListener('keydown', e => {
+    switch (e.key.toLowerCase()) {
+        case " ": 
+        case "k":
+            togglePlay()
+            break
+    }
+})
+
+function togglePlay() {
+    video.paused ? video.play() : video.pause()
+}
+
+video.addEventListener('play', () =>{
+    videoContainer.classList.remove('paused')
+})
+video.addEventListener('pause', () => {
+    videoContainer.classList.add('paused')
+})
